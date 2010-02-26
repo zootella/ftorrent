a0f55068d3c0c0945eab4a7e04271df9f5265f94
Kevin Faaborg <zootella@zootella.com>
2010-02-26 14:05:24 -0500

    visual studio made rc and h

--- resource.h
+++ resource.h
@@ -1,21 +1,15 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Backup.rc
+// Used by ltorrent.rc
 //
-#define IDR_MENU2                       103
-#define ID_TASK_DELETE                  40001
-#define ID_TASK_COPY                    40002
-#define ID_TASK_COMPARE                 40003
-#define ID_TASK_UPDATE                  40004
-#define ID_TASK_UPDATECOMPARE           40005
-#define ID_TEST                         40006
+#define IDI_ICON1                       101
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
