66d526227a9f09c35ac27562b08d03f52e63fbbf
Kevin Faaborg <zootella@zootella.com>
2010-03-03 14:54:53 -0500

    before probably breaking the menu again

--- resource.h
+++ resource.h
@@ -2,18 +2,21 @@
 // Microsoft Visual C++ generated include file.
 // Used by ltorrent.rc
 //
-#define ID_TASK_DELETE                  40001
-#define ID_TASK_COPY                    40002
-#define ID_TASK_COMPARE                 40003
-#define ID_TASK_UPDATE                  40004
-#define ID_TASK_UPDATECOMPARE           40005
+#define ID_TASK_TEST                    40001
+#define ID_TASK_OPEN                    40002
+#define ID_TASK_ADD                     40003
+#define ID_TASK_NEW                     40004
+#define ID_TASK_HELP                    40005
+#define ID_TASK_ABOUT                   40006
+#define ID_TASK_OPTIONS                 40007
+#define ID_TASK_EXIT                    40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
