dc6320d33084cbd8fe80e91e37fa1e23fb09262c
Kevin Faaborg <zootella@zootella.com>
2010-11-24 15:55:37 -0500

    column and customize

--- bench.cpp
+++ bench.cpp
@@ -6,39 +6,20 @@ extern app App; // Access global object
 
 
 
+//TODO make right click menu on headers Customize... and modeless dialog box with checkboxes for each column
 
-//TODO try adding and removing icons instead of setting them to and from blank
-//TODO see if you still need the add and then remove the first column workaround
-
-//new list view design
-//let the control keep its own data, don't switch to owner held data
-//keep a copy of all the data in the control yourself right behind it
-//use that copy to tell when a cell goes out of date, you don't need to query the control for its text at all anymore
-//plan for the future feature of the user hides and drags columns
-//the key for a row is its param, look this up to lead to a row index number 0+
-//the key for a columns is read title, look this up to lead to a column index number 0+
+// Column
+//title
+//factorywidth
+//factoryshow
+//width
+//place
+//show
 
 
 
 
 
-
-
-
-
-//todo
-//have a map of CString to Cell in Torrent, use that to monitor and update what's onscreen
-//then you don't need text from cell anymore, comment it out
-
-//make teh files list in the tab below
-//set it up to load Fruit items thta have different properties
-//test adding and removing columns and rows this way
-//right click remove fruit, test adds another group of them
-
-
-//have this take two cells, what's there and what's current, and then update the display and the cells if they are different
-//code Cell::Same() for this purpose
-//have compose functions take a pointer to a cell, not return one, so data isn't copied as much
 
 
 
@@ -51,10 +32,6 @@ int stage = 1;
 void Test() {
 
 	if (stage == 1) { log(L"stage 1"); stage = 2;
-
-		//no, all you need to do for the workaround is never add with index 0, always add with 1+ index
-		//now all you need to see is how to add a column between other columns, to add it where the user right clicked or in the order of the ones on the menu
-		//when floating over an item in the columns menu of checkboxes, have status text tell the user what that column means
 
 		ColumnAdd(App.window.files, L"Column A", 100, true);
 		ColumnAdd(App.window.files, L"Column B", 110, true);
