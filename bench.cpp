9131e52d5b6183d0d112472253c0158a9d7c00e0
Kevin Faaborg <zootella@zootella.com>
2010-12-05 18:37:45 -0500

    returing reference works

--- bench.cpp
+++ bench.cpp
@@ -16,17 +16,28 @@ void Test() {
 
 	Torrent t;
 
-	Cell a = t.GetCell(L"a");
-	
+	Cell &a = t.GetCell(L"a");
 
-	Cell b = t.GetCell(L"b");
+	a.text = L"a value 1";
+	log(a.text);
+
+	Cell &a2 = t.GetCell(L"a");
+	log(a2.text);
+	
+	a2.text = L"a value 1 edtied";
+	log(a.text);
+	log(a2.text);
+
+	/*
+	Cell &b = t.GetCell(L"b");
 
 	a.text = L"a value 1";
 
-	Cell a2 = t.GetCell(L"a");
+	Cell &a2 = t.GetCell(L"a");
 
 	log(a.text);
 	log(a2.text);
+	*/
 
 
 
