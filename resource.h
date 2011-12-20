d75bd5fa9cdefde6a5faf13a2ef21b5f1fe52c97
Kevin Faaborg <zootella@zootella.com>
2011-12-20 14:01:18 -0500

    before renumbering identifiers

--- resource.h
+++ resource.h
@@ -5,17 +5,17 @@
 #define IDC_BROWSE                      1013
 #define IDC_FOLDER                      1015
 #define IDC_EDIT                        1021
-#define IDC_LIST                        1040
-#define IDC_ADD                         1041
-#define IDC_DELETE                      1043
-#define IDC_CONNECTDIAL                 1051
-#define IDC_CONNECTWAIT                 1052
-#define IDC_INTERNET                    1054
-#define IDC_SUB                         1056
-#define IDC_CONNECTUSE                  1067
-#define IDC_GET                         1073
-#define IDC_GETTEXT                     1074
-#define IDC_GETSPIN                     1075
+#define IDC_LIST                        1040//unused
+#define IDC_ADD                         1041//unused
+#define IDC_DELETE                      1043//unused
+#define IDC_CONNECTDIAL                 1051//unused
+#define IDC_CONNECTWAIT                 1052//unused
+#define IDC_INTERNET                    1054//unused
+#define IDC_SUB                         1056//unused
+#define IDC_CONNECTUSE                  1067//unused
+#define IDC_GET                         1073//unused
+#define IDC_GETTEXT                     1074//unused
+#define IDC_GETSPIN                     1075//unused
 #define IDC_ASSOCIATE                   1078
 #define IDC_LABEL                       1083
 #define ID_TASKBAR_RESTORE              40001
@@ -28,13 +28,31 @@
 #define ID_TOOLS_ABOUT                  40008
 #define ID_TOOLS_HELP                   40009
 #define ID_TOOLS_EXIT                   40010
+#define ID_TORRENT_PROPERTIES           40011
+#define ID_TRACKER_PROPERTIES           40012
+#define ID_FILE_PROPERTIES              40013
+#define ID_TORRENT_OPENCONTAININGFOLDER 40014
+#define ID_TORRENT_COPYMAGNETLINK       40015
+#define ID_TORRENT_SAVETORRENTAS        40016
+#define ID_TORRENT_START                40017
+#define ID_TORRENT_PAUSE                40018
+#define ID_TORRENT_STOP                 40019
+#define ID_TORRENT_REMOVE               40020
+#define ID_TORRENT_REMOVEANDDELETEDATA  40021
+#define ID_PEER_ADDPEER                 40022
+#define ID_PIECE_PROPERTIES             40023
+#define ID_FILE_OPENCONTAININGFOLDER    40024
+#define ID_FILE_HIGHPRIORITY            40025
+#define ID_FILE_NORMALPRIORITY          40026
+#define ID_FILE_LOWPRIORITY             40027
+#define ID_FILE_DON                     40028
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_COMMAND_VALUE         40029
 #define _APS_NEXT_CONTROL_VALUE         1086
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
